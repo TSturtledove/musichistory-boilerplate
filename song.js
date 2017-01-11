9aeb37b8dc922208dc947e575a33e765aa43ce40
TSturtledove <roberttortorelli@yahoo.com>
2017-01-11 07:48:35 -0600

    adjusting for music history 2

---
+++ song.js
@@ -0,0 +1,13 @@
+var songs = [];
+
+songs[songs.length] = "This is song one - by That guy on the album Who knows";
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[songs.length] = "Hey another song - by This guy on the album What";
+
+console.log(songs)
+
+// look into splice and replace to clean up the strings (also concat and stuff)
